7ac027c4540b5b345c8e7c2a5d92601bfcc06e64
Dziuda.Alicja <215713@edu.p.lodz.pl>
2020-06-09 17:37:12 +0200

    prepar test class part1

--- testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
@@ -2,14 +2,37 @@ package edu.iis.mto.testreactor.dishwasher;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import edu.iis.mto.testreactor.dishwasher.engine.Engine;
+import edu.iis.mto.testreactor.dishwasher.pump.WaterPump;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 public class DishWasherTest {
 
     @Test
     public void itCompiles() {
         assertThat(true, Matchers.equalTo(true));
+    }
+
+    @Mock
+    DirtFilter dirtFilter;
+
+    @Mock
+    Door door;
+
+    @Mock
+    Engine engine;
+
+    @Mock
+    WaterPump waterPump;
+
+    DishWasher dishWasher;
+
+    @BeforeEach
+    public void setUp(){
+        dishWasher = new DishWasher(waterPump,engine,dirtFilter,door);
     }
 
 }
